af28285817f330c51fab2d5f3ba8077dc9b9808b
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-01 02:23:12 +0300

    edit temp for offer , requests

--- middleware/is-signed-in.js
+++
@@ -1,6 +0,0 @@
-const isSignedIn = (req, res, next) => {
-    if (req.session.user) return next()
-    res.redirect('/auth/sign-in')
-}
-
-module.exports = isSignedIn
--- middleware/pass-user-to-view.js
+++
@@ -1,6 +0,0 @@
-const passUserToView = (req, res, next) => {
-    res.locals.user = req.session.user ? req.session.user : null
-    next()
-}
-
-module.exports = passUserToView
